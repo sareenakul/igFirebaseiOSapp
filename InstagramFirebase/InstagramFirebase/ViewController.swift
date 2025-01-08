a71408ac43a788a4931fab9a5c142d79ebec0233
Akul Sareen <sareen.akul@gmail.com>
2025-01-08 13:41:08 -0500

    Added the height and width param in my anchor extension

--- InstagramFirebase/InstagramFirebase/ViewController.swift
+++ InstagramFirebase/InstagramFirebase/ViewController.swift
@@ -94,26 +94,33 @@ class ViewController: UIViewController {
         
         
         
-        stackView.anchor(top: addPhotoButton.bottomAnchor, bottom: nil, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 20, paddingBottom: 0, paddingLeft: 40, paddingRight: -40)
+        stackView.anchor(top: addPhotoButton.bottomAnchor, bottom: nil, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 20, paddingBottom: 0, paddingLeft: 40, paddingRight: -40, width: 0, height: 200)
     }
 }
 
 extension UIView{
-    func anchor(top: NSLayoutYAxisAnchor?, bottom: NSLayoutYAxisAnchor?, left: NSLayoutXAxisAnchor?, right: NSLayoutXAxisAnchor?, paddingTop: CGFloat, paddingBottom: CGFloat, paddingLeft: CGFloat, paddingRight: CGFloat){
+    func anchor(top: NSLayoutYAxisAnchor?, bottom: NSLayoutYAxisAnchor?, left: NSLayoutXAxisAnchor?, right: NSLayoutXAxisAnchor?, paddingTop: CGFloat, paddingBottom: CGFloat, paddingLeft: CGFloat, paddingRight: CGFloat, width: CGFloat, height: CGFloat){
         
+        //Unwrap to be learnt
+        if let top = top{
+            self.topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        if let bottom = bottom{
+            self.bottomAnchor.constraint(equalTo: bottom, constant: paddingBottom).isActive = true
+        }
+        if let left = left{
+            self.leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        if let right = right{
+            self.rightAnchor.constraint(equalTo: right, constant: paddingRight).isActive = true
+        }
+        if width != 0{
+            self.widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
         
-            if let top = top{
-                self.topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
-            }
-            if let bottom = bottom{
-                self.bottomAnchor.constraint(equalTo: bottom, constant: paddingBottom).isActive = true
-            }
-            if let left = left{
-                self.leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
-            }
-            if let right = right{
-                self.rightAnchor.constraint(equalTo: right, constant: paddingRight).isActive = true
-            }
+        if height != 0{
+            self.heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
     }
 }
 
