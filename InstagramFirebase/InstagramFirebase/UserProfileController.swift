c0c78207b7ac51c2bdbde6d026e17bf9415ddca9
Akul Sareen <sareen.akul@gmail.com>
2025-02-09 17:42:06 -0500

    Worked on fetchUser()

--- InstagramFirebase/InstagramFirebase/UserProfileController.swift
+++ InstagramFirebase/InstagramFirebase/UserProfileController.swift
@@ -5,6 +5,9 @@
 //  Created by Akul Sareen on 2025-02-06.
 //
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
 
 class UserProfileController: UICollectionViewController {
     
@@ -12,7 +15,19 @@ class UserProfileController: UICollectionViewController {
         super.viewDidLoad()
         //tabBarController?.tabBar.isHidden = false
         
-        collectionView.backgroundColor = .green
+        collectionView.backgroundColor = .white
+        navigationItem.title = Auth.auth().currentUser?.uid
         
+        fetchUser()
+        
+    }
+    
+    fileprivate func fetchUser(){
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        Database.database().reference().child("users").child(uid).observeSingleEvent(of: .value, with: {(snapshot) in print(snapshot.value ?? "")}) {
+            (error) in print("Failed to fetch user: \(error.localizedDescription)")
+        }
     }
 }
